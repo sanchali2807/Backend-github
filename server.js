8d5b0b382574febf654b934e7f3f82a51bd52ebd
Sanchali <sanchaligarg028@gmail.com>
2025-02-28 12:07:14 +0530

    done

--- server.js
+++ server.js
@@ -3,14 +3,30 @@
 
 const express=require("express");
 const app=express();
+
+//miuddleware to convert url encoded data 
+// app.use so that it works every time on every request
+app.use(express.urlencoded({extended:true}))
+//now an array to store data
+let  userData =[]
+
+
 app.get("/signup",(req,res)=>
 {
     res.sendFile(__dirname+"/signup.html")
 })
 app.post("/signup",(req,res)=>
 {
-   let {username,useremail,userpassword}=req.body;
-   console.log(username,useremail,userpassword);
+//    let {username,useremail,userpassword}=req.body;
+//    console.log(username,useremail,userpassword);
+let{username , useremail, userpassword}=req.body;
+let NewUser = {
+   name : username,
+   email : useremail,
+   password : userpassword
+}
+ userData.push(NewUser);
+ res.send(userData);
 }
 )
 app.listen((3000),()=>
