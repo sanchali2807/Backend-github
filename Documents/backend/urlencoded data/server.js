ce12fee71ae7d673a26a61a1efc735d73ec3c3c2
Sanchali <sanchaligarg028@gmail.com>
2025-03-05 13:26:24 +0530

    done

---
+++ Documents/backend/urlencoded data/server.js
@@ -0,0 +1,30 @@
+commit on 27 feb:const express = require("express");
+const app= express();
+
+//moddleware to convert urlencoded data
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+let userData =[];
+
+app.get("/signup",(req,res)=>{
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/signup",(req,res)=>{
+    let {username,useremail,userpassword} = req.body;
+    let newUser={
+        name:username,
+        email:useremail,
+        password:userpassword
+    }
+    userData.push(newUser);
+
+    res.send(userData);
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is started");
+})
