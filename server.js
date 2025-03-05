205e3c50a2353b15da7e6aff32dee06fbd4bf306
Sanchali <156992815+sanchali2807@users.noreply.github.com>
2025-03-05 19:44:01 +0530

    Add files via upload
    new commits

--- server.js
+++ server.js
@@ -1,35 +1,30 @@
-// 20 feb 2025    need to make this commit in github
-// we need to make a get request for this page to show up
+const express = require("express");
+const app= express();
 
-const express=require("express");
-const app=express();
+//moddleware to convert urlencoded data
 
-//miuddleware to convert url encoded data 
-// app.use so that it works every time on every request
 app.use(express.urlencoded({extended:true}))
-//now an array to store data
-let  userData =[]
+app.use(express.json())
 
+let userData =[];
 
-app.get("/signup",(req,res)=>
-{
+app.get("/signup",(req,res)=>{
     res.sendFile(__dirname+"/signup.html")
 })
-app.post("/signup",(req,res)=>
-{
-//    let {username,useremail,userpassword}=req.body;
-//    console.log(username,useremail,userpassword);
-let{username , useremail, userpassword}=req.body;
-let NewUser = {
-   name : username,
-   email : useremail,
-   password : userpassword
-}
- userData.push(NewUser);
- res.send(userData);
-}
-)
-app.listen((3000),()=>
-{
-    console.log("server started");
+
+app.post("/signup",(req,res)=>{
+    let {username,useremail,userpassword} = req.body;
+    let newUser={
+        name:username,
+        email:useremail,
+        password:userpassword
+    }
+    userData.push(newUser);
+
+    res.send(userData);
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is started");
 })
