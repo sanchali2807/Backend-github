34aa531ffe7d112b2936cb714d79d0463baebd21
Sanchali <sanchali280705@gmail.com>
2026-01-22 20:22:34 +0530

    Initial commit with folder

---
+++ app.js
@@ -0,0 +1,26 @@
+// // let a = 10;
+// // let b = 20;
+// function add(x,y){
+//     return x+y;
+// }
+// function subtract (x,y){
+//     return x-y;
+// }
+// function divide(x,y){
+//     return x/y;
+// }
+// function multiply(x,y){
+//     return x*y;
+// }
+// process.stdin.on("data", (data) => {
+//     console.log("Input:", data.toString().trim());
+// });
+
+// console.log("Addition:", add(a, b));
+// console.log("Subtraction:", subtract(a, b));
+// console.log("Multiplication:", multiply(a, b));
+// console.log("Division:", divide(a, b));
+
+function wait(){
+    
+}
